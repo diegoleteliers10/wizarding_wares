0b878e2ce4c5d0dfedbf937df11c73d7e5ac4df8
Diego Letelier <diegoleteliers10@gmail.com>
2023-06-12 13:57:12 -0400

    feature/addAuth0

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const server = express();
 const morgan = require("morgan");
+const { auth } = require('express-openid-connect');
 
 const router = require("./routes/index");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'D1XZMq2ldqwrVRQF5lHHrk5phLO2zTYz',
+  issuerBaseURL: 'https://wizarding-wares.us.auth0.com'
+};
 
 server.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -18,6 +28,7 @@ server.use((req, res, next) => {
 // Middlewares
 server.use(express.json());
 server.use(morgan("dev"));
+server.use(auth(config));
 // server.use('/rickandmorty', router); --> NO EXISTE ESA RUTA, DA ERROR
 
 // Ruta Principal Provisoria
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,2 +1,4 @@
-
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// }); //ruta principal del auth
 
