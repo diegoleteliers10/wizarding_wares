ded2aec1c530ab7781c68d58d43b4652b8b1a95e
marcosmorrone99 <momarcos99@gmail.com>
2023-06-13 17:58:54 -0300

    esqueleto footer

---
+++ Client/src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    Aca va el logo
+                </div>
+                <div>
+                    <h4>Información</h4>
+                    <a href="/nosotros"/>
+                    <p>Nosotros</p>
+                    <a href="/faq"/>
+                    <p>FAQ</p>
+                </div>
+                <div>
+                <h4>Legal</h4>
+                    <a href="/terminosYCondiciones"/>
+                    <p>Términos y Condiciones</p>
+                    <a href="/politicaDePrivacidad"/>
+                    <p>Política de Privacidad</p>
+                </div>
+            </div>
+            <div>
+                2023 - Wizarding Wares | All rights reserved
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
