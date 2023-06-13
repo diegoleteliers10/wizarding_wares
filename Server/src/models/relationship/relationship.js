e306e2fd23fd55bdb3322e000fa6b61a87d76989
KailyKinG <kailyking@gmail.com>
2023-06-13 15:36:54 -0400

    feature/modelosPurchaseYStatus(Server/modelosPurchaseYStatus): Se relacionaron los modelos y se exportaron

--- Server/src/models/relationship/relationship.js
+++ Server/src/models/relationship/relationship.js
@@ -1,14 +1,20 @@
 // 1_ Aqui importaremos nuestros modelos listos y conectados a sequelize
 const Product = require("../Product.model");
 const Categorie = require("../Categorie.model");
+const Purchase = require("../Purchase.model");
+const Status = require("../Status.model");
 
 // 2_ Haremos las relaciones entre ellos
 Categorie.hasMany(Product);
 Product.belongsTo(Categorie);
+Status.hasMany(Purchase);
+Purchase.belongsTo(Status);
 
 // 3_ Exportremos Los Modelos Con La Relacion Entre Estos Integrada
 module.exports = {
 	Product,
 	Categorie,
+	Purchase,
+	Status,
 };
 // Y sera desde aqui que tomaremos los modelos para hacer el trabajo con rutas y controllers
