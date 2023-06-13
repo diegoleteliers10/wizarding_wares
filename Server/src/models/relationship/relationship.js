ce7044203a5bdb21177e0bf7c1b9cd9eb845a1a1
KailyKinG <kailyking@gmail.com>
2023-06-13 16:36:17 -0400

    feature/completandoRelatioships(Server/completandoRelatioships): Se Ralizaron las relaciones totales entre los modelos terminados, solo falta El modelo 'Review'

--- Server/src/models/relationship/relationship.js
+++ Server/src/models/relationship/relationship.js
@@ -5,16 +5,23 @@ const Purchase = require("../Purchase.model");
 const Status = require("../Status.model");
 const Role = require("../Roles.model");
 const User = require("../Users.model");
-
+const Address = require("../Address.model");
 
 // 2_ Haremos las relaciones entre ellos
+Role.hasMany(User);
+User.belongsTo(Role);
 Categorie.hasMany(Product);
 Product.belongsTo(Categorie);
 Status.hasMany(Purchase);
 Purchase.belongsTo(Status);
-Role.hasMany(User);
-User.belongsTo(Role);
-
+User.hasMany(Purchase);
+Purchase.belongsTo(User);
+Address.hasMany(Purchase);
+Purchase.belongsTo(Address);
+Product.belongsToMany(Purchase, { through: "Purchase_Product" });
+Purchase.belongsToMany(Product, { through: "Purchase_Product" });
+User.belongsToMany(Address, { through: "User_Address" });
+Address.belongsToMany(User, { through: "User_Address" });
 
 // 3_ Exportremos Los Modelos Con La Relacion Entre Estos Integrada
 module.exports = {
@@ -24,5 +31,6 @@ module.exports = {
 	Status,
 	Role,
 	User,
+	Address,
 };
 // Y sera desde aqui que tomaremos los modelos para hacer el trabajo con rutas y controllers
