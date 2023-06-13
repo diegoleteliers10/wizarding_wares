558b69aa14808d41adbc8c4670b576abc5a9800c
Diego Letelier <diegoleteliers10@gmail.com>
2023-06-13 14:21:34 -0400

    feat/addingUserANDRoleModels

---
+++ Server/src/models/Users.model.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define("user", {
+	userId: {
+		type: DataTypes.UUID,
+		primaryKey: true,
+		allowNull: false,
+	},
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [6, 20],
+      isAlphanumeric: true,
+    }
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: null,
+  },
+}, {
+  timestamps: true,
+  paranoid: true, // habilita el borrado lógico
+});
+
+module.exports = User;
