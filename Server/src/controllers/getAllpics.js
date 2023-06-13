df1a297bd7c630ca110269399eaaf6b76791fd06
Diego Letelier <diegoleteliers10@gmail.com>
2023-06-13 18:27:31 -0400

    feat/addRoutProductPics

--- Server/src/controllers/getAllpics.js
+++ Server/src/controllers/getAllpics.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 
+const {CLOUD_NAME, KEY_CLOUD, SECRET_CLOUD  } = process.env;
+
 const getAllPics = async (req,res) => {
   try {
     // Buscamos el perro por su ID
-    const imagenes = await axios.get(`https://398427172815388:qwQCCHXyHDGW6bW1BILuEFSJYew@api.cloudinary.com/v1_1/dfjzdxfop/resources/image`)
+    const imagenes = await axios.get(`https://${KEY_CLOUD}:${SECRET_CLOUD}@api.cloudinary.com/v1_1/${CLOUD_NAME}/resources/image`)
     const allImg= imagenes.data.resources;
 
     res.status(200).send(allImg)
