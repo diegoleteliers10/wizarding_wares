bec5f1b23d7001961d3bbe4efb35a260954731c6
emiferrari2001 <emiferrari2001@gmail.com>
2023-06-14 20:31:09 -0300

    correccion duplicado en navBar

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -5,15 +5,10 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar = () => {
     return(
          <div>
-         <button className="btn btn--svg-small">Home</button>
-         <SearchBar/>
-         <button className="btn btn--svg-small">Login</button>
-         <button className="btn btn--svg-small">Register</button>
-         <Navbar bg="light" expand="lg">
-         <button>Home</button>
-         <SearchBar/>
-         <button>Login</button><button>Register</button>
-         </Navbar>
+            <SearchBar/>
+            <button className="btn btn--svg-small">Home</button>
+            <button className="btn btn--svg-small">Login</button>
+            <button className="btn btn--svg-small">Register</button>
         </div>
     )
 }
