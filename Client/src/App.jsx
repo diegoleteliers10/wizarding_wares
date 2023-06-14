2fafc71c7ce473eac37fb094d458912702a1398e
valen4009 <valentinosuarez15@gmail.com>
2023-06-14 11:07:27 -0300

    delete de Landing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './views/home/Home'
-import LandingPage from './views/LandingPage/LandingPage'
 import Detail from './components/Detail/Detail'
 
 function App() {
@@ -9,8 +8,7 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<LandingPage/>} />
-        <Route path="/home" element={<Home/>} />
+        <Route path='/' element={<Home/>} />
         <Route path='/:id' element={<Detail/>} />
       </Routes>
     </div>
