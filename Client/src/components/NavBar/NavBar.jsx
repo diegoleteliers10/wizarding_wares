a7d43850ad8e38c03d8dd61332cf22ea1657c4fa
marcosmorrone99 <momarcos99@gmail.com>
2023-06-13 22:24:51 -0300

    navbar esqueleto

---
+++ Client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import SearchBar from "../SearchBar/SearchBar"
+
+
+const NavBar = () => {
+    return(
+         <div>
+         <button>Home</button>
+         <SearchBar/>
+         <button>Login</button><button>Register</button>
+        </div>
+    )
+}
+
+export default NavBar;
