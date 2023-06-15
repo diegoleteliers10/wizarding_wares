b42b6ba1c58a6b4fd989e745b198bda7c8d8ed5b
KailyKinG <kailyking@gmail.com>
2023-06-15 15:47:27 -0400

    feature/routeToUpdateACategory(Server/routeToUpdateACategory): Se creo la ruta put(/categorie, () => {}) y el controllers updateACategory. Para Modificar una Categorie

--- Server/src/controllers/categorieControllers.js
+++ Server/src/controllers/categorieControllers.js
@@ -2,7 +2,7 @@ const { arrojarError, validateString } = require("../utils/utils");
 const {Categorie} = require("../models/relationship/relationship");
 
 
-// PEDIR TODAS LAS CATEGORIAS
+// *** PEDIR TODAS LAS CATEGORIAS ***
 const getAllCategories = async () => {
   // Primero validamos si tenemos datos en Categorie
   const exists = await Categorie.findAll();
@@ -17,7 +17,10 @@ const getAllCategories = async () => {
 };
 
 
-// CREAR UNA CATEGORIA
+// ***************************************************************************************
+
+
+// *** CREAR UNA CATEGORIA ***
 const crearCategoria = async (name) => {
   // Si no pasan un dato, se arroja un error
   if(!name) {
@@ -50,7 +53,57 @@ const crearCategoria = async (name) => {
   return `Categorie ${name}. Created Successfully`;
 };
 
+
+// ***************************************************************************************
+
+// *** MODIFICAR UNA CATEGORIA ***
+const updateACategory = async (name, modify) => {
+
+  // Primero verificamos si tenemos o no categorias
+  const exists = await Categorie.findAll();
+
+  // En el caso de no tener categorias a modificar lanzamos un error
+  if(!exists.length){
+    arrojarError("No Tienes Categorias Para Modificar");
+  }
+
+  // Verificamos si nos pasan argumento o no
+  (!name || !modify) && arrojarError("Parametros Necesarios Incompletos");
+
+  // Validamos si los argumentos ingresados son de tipo String
+  const validName = validateString(name);
+  const validModify = validateString(modify);
+
+  // Si los argumentos no cumplen la condicion de solo Letras Y Espacios. Lanzamos un error
+  if(!validName || !validModify){
+    arrojarError("Parametros; Deben De Ser Solo Letras Del ABC y Espacios");
+  }
+
+  // Buscamos la categoria que queremos modificar
+  const categorie = await Categorie.findOne({
+    where: {
+      name: name
+    }
+  });
+
+  // Si no existe la categoria lanzamos un error
+  if(categorie == null){
+    arrojarError(`Categorie ${name}; No Existe En Tu DB`);
+  }
+
+  // Caso contrario modificamos el nombre de nuestra categoria
+  const newNameCategory = await Categorie.update({name: modify},{
+    where: {
+      name: name
+    }
+  });
+
+  // return newNameCategory;
+  return `Modification Made Successfully`;
+};
+
 module.exports = {
   crearCategoria,
   getAllCategories,
+  updateACategory,
 };
