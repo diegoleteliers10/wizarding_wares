eab0223512702a135f5aa4786738a333667f2d6f
marcosmorrone99 <momarcos99@gmail.com>
2023-06-15 17:57:17 -0300

    panel del dashboard terminado, sin funcionalidad

---
+++ Client/src/components/Admin/SideBar/SideBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiUser, FiGrid, FiGift } from "react-icons/fi";
+
+const SideBar = () => {
+
+  return (
+    <div className="border rounded py-4 px-3 bg-gray-100 flex flex-col items-center justify-center h-full">
+      <div className="mt-4">
+        <h4>Panel de Admin</h4>
+      </div>
+      <div className="flex flex-col justify-center space-y-4 my-4">
+        <button className="bg-gray-100 rounded-full hover:bg-gray-400">
+          <div className="flex items-center justify-center py-2 px-4">
+            <FiUser className="mr-2" /> Usuarios
+          </div>
+        </button>
+        <button className="bg-gray-100 rounded-full hover:bg-gray-400">
+          <div className="flex items-center justify-center py-2 px-4">
+            <FiGrid className="mr-2" /> Productos
+          </div>
+        </button>
+        <button className="bg-gray-100 rounded-full hover:bg-gray-400">
+          <div className="flex items-center justify-center py-2 px-4">
+            <FiGift className="mr-2" /> Pedidos
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
+
+
+
+
