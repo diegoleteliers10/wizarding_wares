0467a511c67fa92a231f1e57bd1485a9004b8343
Juan Schcolnik <schconlikj@gmail.com>
2023-06-15 12:42:44 -0300

    Admin CMS

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './views/home/Home'
 import Detail from './components/Users/Detail/Detail'
+import Admin from './views/Admin/Admin'
+import CreateProduct from './components/Admin/Products/Create/CreateProduct'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/:id' element={<Detail/>} />
+        <Route path='/admin' element={<Admin/>} />
+        <Route path='/admin/create' element={<CreateProduct/>} />
       </Routes>
     </div>
   )
---
+++ Client/src/components/Admin/Products/Create/CreateProduct.jsx
@@ -0,0 +1,10 @@
+
+const CreateProduct = () => {
+    return(
+        <div>
+            <h2>Acá se crean los productos</h2>
+        </div>
+    )
+}
+
+export default CreateProduct;
