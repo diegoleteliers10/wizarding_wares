fa7b4b62def6a1bc3838989a9d9dedd9e6b5bc47
GastonHaro <92901493+GastonHaro@users.noreply.github.com>
2023-06-17 17:09:03 -0300

    fix/getDetailProduct
    se agrego stock a detalles y re se arreglo un error

--- Server/src/controllers/getDetailProduct.controller.js
+++ Server/src/controllers/getDetailProduct.controller.js
@@ -1,10 +1,10 @@
 const { Product, Review } = require('../models/relationship/relationship');
 
-const getDetailProduct = (req, res) => {
+const getDetailProduct = async (req, res) => {
     const { id } = req.params;
     try {
-        const product = Product.findByPk(id, {
-            attributes: ['name', 'description', 'image', 'price', ],
+        const product = await Product.findByPk(id, {
+            attributes: ['name', 'description', 'image', 'price', 'stock'],
             include: [{
                 model: Review,
                 attributes: ['rating', 'comment'],
