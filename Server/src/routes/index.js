76aac4ee5c4c09bd5b1e843b447e86878024eebc
GastonHaro <gastonharo97@gmail.com>
2023-06-17 19:16:09 -0300

    se agrego ruta para el buscado de producto por nombre

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -4,7 +4,7 @@ const getAllUsers = require('../controllers/getAllUsers.controller');
 const createProduct = require('../controllers/createProduct.controller');
 const editProduct = require('../controllers/editProduct.controller')
 const deleteProduct = require('../controllers/deleteProduct.controller')
-
+const searchCountryByName = require('../controllers/getProductByName.controller')
 const filteredProduct = require('../controllers/filteredProducts.controller')
 
 const getDetailProduct = require('../controllers/getDetailProduct.controller')
@@ -14,14 +14,14 @@ const router = Router();
 
 //user routes
 
-
 //user and admin routes
+router.get('/searchProduct', searchCountryByName)
 router.get('/allproducts', getAllProducts)
 router.get('/filteredProducts', filteredProduct)
+router.get('/detailProduct/:id', getDetailProduct)
 
 //admins routes
 router.get('/allproducts', getAllUsers)
-router.get('/detailProduct/:id', getDetailProduct)
 router.post('/productCreated',createProduct)
 router.put('/editProduct/:id', editProduct)
 router.delete('/deleteProduct/:id', deleteProduct)
