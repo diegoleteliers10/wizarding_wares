3073cc11698238688ce8dac8d41d0eacecc74e7e
emiferrari2001 <emiferrari2001@gmail.com>
2023-06-19 13:17:55 -0300

    mantiene estado al refrescar componente filtros

--- Client/src/components/Users/FilterStore/FilterStore.jsx
+++ Client/src/components/Users/FilterStore/FilterStore.jsx
@@ -3,59 +3,83 @@ import { startTransition, useEffect } from "react";
 import { RxCaretSort } from 'react-icons/rx';
 import styles from "./FilterStore.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { filterCategory, getProducts, filterPrice } from "../../../redux/userSlice";
+import {
+  filterCategory,
+  getProducts,
+  filterPrice,
+  sortByNameAscending,
+  sortByNameDescending,
+  sortByPriceAscending,
+  sortByPriceDescending,
+} from '../../../redux/userSlice';
 
 
 function FilterStore(props) {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const currCategory = useSelector(state => state.user.filterCategory)
-  const currPrice = useSelector(state => state.user.filterPrice)
+  const currPrice = useSelector(state => state.user.filterPrice);
+  const currSort = useSelector(state => state.user.sort)
   //console.log(pathname);
 
-  function handleFilterChange(event){
-    const filter = event.target.value;
-    //console.log(filter)
-    dispatch(filterCategory(filter))
-  }
-
-  function handlePriceFilter(){
-    const minPrice = document.getElementById('minPrice').value;
-    const maxPrice = document.getElementById('maxPrice').value;
-    if(parseInt(minPrice) >= parseInt(maxPrice) || parseInt(maxPrice) <= parseInt(minPrice) ) {
-      alert('Revise los valores')
-      return;
-    }
-    dispatch(filterPrice([minPrice, maxPrice]))
-  }
-
-  function handleReset(){
-    dispatch(getProducts())
-    document.getElementById('filterCategory').value = "Categoría";
-    document.getElementById('orderByPrice').value = "Precio";
-    document.getElementById('orderByRating').value = "Calificación";
-    document.getElementById('orderByName').value = "Orden";
-    document.getElementById('minPrice').value = '';
-    document.getElementById('maxPrice').value = '';
-  }
-
   useEffect(()=> {
-    console.log(currCategory)
-    if(currCategory) {
-      document.getElementById('filterCategory').value = currCategory;
-    }
+    if(currCategory) document.getElementById('filterCategory').value = currCategory;
     if(currPrice.length){
       console.log(currPrice)
        document.getElementById('minPrice').value = currPrice[0];
        document.getElementById('maxPrice').value = currPrice[1];
     }
+    if(currSort[1] !== 'SortP') document.getElementById('orderByPrice').value = currSort[1];
+    if(currSort[2] !== 'SortN') document.getElementById('orderByName').value = currSort[2];
   }, [])
-  
+
+  function handleFilterChange(event) {
+    const filter = event.target.value;
+      dispatch(filterCategory(filter));
+      document.getElementById('orderByPrice').value = 'SortP';
+    document.getElementById('orderByRating').value = 'SortR';
+    document.getElementById('orderByName').value = 'SortN';
+  }
+  function handleSortChange(event) {
+    const sort = event.target.value;
+    if (sort === 'nameAscending') {
+      dispatch(sortByNameAscending());
+  }else if(sort === 'nameDescending'){
+    dispatch(sortByNameDescending())
+  }else if(sort === 'priceHighToLow'){
+    dispatch(sortByPriceAscending())
+  }else if(sort === 'priceLowToHigh'){
+    dispatch(sortByPriceDescending())
+  }
+}
+  function handlePriceFilter() {
+    const minPrice = document.getElementById('minPrice').value;
+    const maxPrice = document.getElementById('maxPrice').value;
+
+    if (parseInt(minPrice) >= parseInt(maxPrice) || parseInt(maxPrice) <= parseInt(minPrice)) {
+      alert('Revise los valores');
+      return;
+    }
+
+    dispatch(filterPrice([minPrice, maxPrice]));
+  }
+
+  function handleReset() {
+    dispatch(getProducts());
+    document.getElementById('filterCategory').value = 'Categoría';
+    document.getElementById('orderByPrice').value = 'SortP';
+    document.getElementById('orderByRating').value = 'SortR';
+    document.getElementById('orderByName').value = 'SortN';
+    document.getElementById('minPrice').value = '';
+    document.getElementById('maxPrice').value = '';
+  }
 
   return (
     <div className={`mx-auto flex justify-center ${styles.filterContainer}`}>
       <div className={`inline-block ${styles.filterItem}`}>
-        <button className="bg-red-100 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l" onClick={handleReset}>Reset</button>
+        <button className="bg-red-100 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l" onClick={handleReset}>
+          Reset
+        </button>
       </div>
       <div className={`inline-block ${styles.filterItem}`}>
         {/* Orden por calificacion */}
@@ -65,44 +89,43 @@ function FilterStore(props) {
           id="orderByRating"
           className={`bg-white ${styles.customSelect}`} // Use the imported class name
         >
-          <option value="Calificación" hidden>
+          <option value="SortR" hidden>
             Calificación
           </option>
           <option value="rateHighToLow">Mejor puntuación</option>
           <option value="rateLowToHigh">Peor puntuación</option>
         </select>
       </div>
-      {
-        (pathname === '/' || pathname === 'search') && (
-          <div className={`inline-block ${styles.filterItem}`}>
-            {/* Filtro categoria */}
-            <select
-              name="filterCategory"
-              id="filterCategory"
-              className={`bg-white ${styles.customSelect}`}
-              onChange={handleFilterChange}
-            >
-              <option value="Categoría" hidden>
-                Categoría
-              </option>
-              <option value="Golosinas">Golosinas</option>
-              <option value="Indumentaria">Indumentaria</option>
-              <option value="Libros">Libros</option>
-              <option value="Quidditch">Quidditch</option>
-              <option value="Varitas">Varitas</option>
-              <option value="Misceláneas">Misceláneos</option>
-            </select>
-          </div>
-        )
-      }
+      {(pathname === '/' || pathname === 'search') && (
+        <div className={`inline-block ${styles.filterItem}`}>
+          {/* Filtro categoria */}
+          <select
+            name="filterCategory"
+            id="filterCategory"
+            className={`bg-white ${styles.customSelect}`}
+            onChange={handleFilterChange}
+          >
+            <option value="Categoría" hidden>
+              Categoría
+            </option>
+            <option value="Golosinas">Golosinas</option>
+            <option value="Indumentaria">Indumentaria</option>
+            <option value="Libros">Libros</option>
+            <option value="Quidditch">Quidditch</option>
+            <option value="Varitas">Varitas</option>
+            <option value="Misceláneas">Misceláneos</option>
+          </select>
+        </div>
+      )}
       <div className={`inline-block ${styles.filterItem}`}>
         {/* Orden por precio */}
         <select
           name="orderByPrice"
           id="orderByPrice"
           className={`bg-white ${styles.customSelect}`} // Use the imported class name
+          onChange={handleSortChange}
         >
-          <option value="Precio" hidden>
+          <option value="SortP" hidden>
             Precio
           </option>
           <option value="priceHighToLow">Mayor precio</option>
@@ -115,18 +138,22 @@ function FilterStore(props) {
           name="orderByName"
           id="orderByName"
           className={`bg-white ${styles.customSelect}`} // Use the imported class name
+          onChange={handleSortChange}
         >
-          <option value="Orden" hidden>Orden</option>
-          <option value="nameAscending">Ascendente</option>
-          <option value="nameDescending">Descendente</option>
+          <option value="SortN" hidden>
+            Orden
+          </option>
+          <option value="nameAscending">Nombre (A-Z)</option>
+          <option value="nameDescending">Nombre (Z-A)</option>
         </select>
       </div>
       <div className={`${styles.dInherit}`}>
-        <input type="number" placeholder="Min" className="bg-white" id="minPrice"/>
-        <input type="number" placeholder="Max" className="bg-white" id="maxPrice"/>
-        <button className="bg-gray-100 h-1/2" onClick={handlePriceFilter}>Buscar</button>
+        <input type="number" placeholder="Min" className="bg-white" id="minPrice" />
+        <input type="number" placeholder="Max" className="bg-white" id="maxPrice" />
+        <button className="bg-gray-100 h-1/2" onClick={handlePriceFilter}>
+          Buscar
+        </button>
       </div>
-      
     </div>
   );
 }
--- Client/src/redux/userSlice.js
+++ Client/src/redux/userSlice.js
@@ -1,5 +1,5 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios  from "axios";
+import { createAsyncThunk, createSlice, createAction } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   products: [],
@@ -7,117 +7,131 @@ const initialState = {
   detail: [],
   display: '',
   filterCategory: false,
-  filterPrice: [],    
-}
+  filterPrice: [],
+  sort: ['SortR', 'SortP', 'SortN']
+};
 
 export const getProducts = createAsyncThunk(
-  'user/getProducts',
+  'user/getProducts', 
   async (_, thunkAPI) => {
-    try {
-      const response = await axios.get('http://localhost:3001/allProducts');
-      return response.data;
-    } catch (error) {
-      console.error('Error al obtener los productos:', error);
-      throw error;
-    }
+  try {
+    const response = await axios.get('http://localhost:3001/allProducts');
+    return response.data;
+  } catch (error) {
+    console.error('Error al obtener los productos:', error);
+    throw error;
   }
-);
+});
 
 export const filterCategory = createAsyncThunk(
-  'user/filterCategory',
+  'user/filterCategory', 
   async (filter, { getState }) => {
-    try {
-      const state = getState();
-      //console.log(state.user.filterPrice);
-      if(state.user.filterPrice.length){
-        const minPrice = state.user.filterPrice[0];
-        const maxPrice = state.user.filterPrice[1];
-        const response = await axios.get(`http://localhost:3001/filteredProducts?category=${filter}&minPrice=${minPrice}&maxPrice=${maxPrice}`);
-        return [filter, response.data];
-      }
-      const response = await axios.get(`http://localhost:3001/filteredProducts?category=${filter}`);
-      //console.log(response.data)
+  try {
+    const state = getState();
+    //console.log(state.user.filterPrice);
+    if(state.user.filterPrice.length){
+      const minPrice = state.user.filterPrice[0];
+      const maxPrice = state.user.filterPrice[1];
+      const response = await axios.get(`http://localhost:3001/filteredProducts?category=${filter}&minPrice=${minPrice}&maxPrice=${maxPrice}`);
       return [filter, response.data];
-    } catch (error) {
-      console.error('Error obtaining filtered products', error);
-      throw error;      
     }
+    const response = await axios.get(`http://localhost:3001/filteredProducts?category=${filter}`);
+    //console.log(response.data)
+    return [filter, response.data];
+  } catch (error) {
+    console.error('Error obtaining filtered products', error);
+    throw error;
   }
-)
+});
 
 export const filterPrice = createAsyncThunk(
   'user/filterPrice',
-  async (prices, { getState }) => {
-    try {
-      const minPrice = prices[0]
-      const maxPrice = prices[1] 
-      const state = getState();
-      if(state.user.filterCategory){
-        const category = state.user.filterCategory;
-        //console.log(category)
-        const response = await axios.get(`http://localhost:3001/filteredProducts?category=${category}&minPrice=${minPrice}&maxPrice=${maxPrice}`);
-      //console.log(response.data);
-      return [minPrice, maxPrice, response.data];
-      }        
-      const response = await axios.get(`http://localhost:3001/filteredProducts?minPrice=${minPrice}&maxPrice=${maxPrice}`);
-      //console.log(response.data);
-      return [minPrice, maxPrice, response.data];
-    } catch (error) {
-      console.error('Error obtaining filtered products', error);
-      throw error;      
-    }
+   async (prices, { getState }) => {
+  try {
+    const minPrice = prices[0];
+    const maxPrice = prices[1];
+    const state = getState();
+    if(state.user.filterCategory){
+      const category = state.user.filterCategory;
+      //console.log(category)
+      const response = await axios.get(`http://localhost:3001/filteredProducts?category=${category}&minPrice=${minPrice}&maxPrice=${maxPrice}`);
+    //console.log(response.data);
+    return [minPrice, maxPrice, response.data];
+    }        
+    const response = await axios.get(`http://localhost:3001/filteredProducts?minPrice=${minPrice}&maxPrice=${maxPrice}`);
+    //console.log(response.data);
+    return [minPrice, maxPrice, response.data];
+  } catch (error) {
+    console.error('Error obtaining filtered products', error);
+    throw error;
   }
-);
+});
 
-//MANEJO DE ESTADOS DE REQUEST Y PAYLOADS
+export const sortByNameAscending = createAction('user/sortByNameAscending');
+export const sortByNameDescending = createAction('user/sortByNameDescending');
+export const sortByPriceAscending = createAction('user/sortByPriceAscending');
+export const sortByPriceDescending = createAction('user/sortByPriceDescending');
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    // Add reducers for additional action types here, and handle loading state as needed
     builder
-    .addCase(getProducts.fulfilled, (state, action) => {
-      state.loading = false;
-      state.products = action.payload;
-      state.filterCategory = false;
-      state.filterPrice = [];
-    })
-    .addCase(getProducts.rejected, (state, action) => {
-      state.loading = false;
-      console.error('Error al obtener los productos:', action.error);
-    })
-    .addCase(getProducts.pending, (state, action) => {
-      state.loading = true
-      console.log(action);
-    })
-    .addCase(filterCategory.fulfilled, (state,action) => {
-      state.loading = false
-      state.products = action.payload[1];
-      state.filterCategory = action.payload[0];
-    })
-    .addCase(filterCategory.rejected, (state,action) => {
-      state.loading = false
-      console.error('Error obtaining filtered products ', action.error);
-    })
-    .addCase(filterCategory.pending, (state,action) => {
-      state.loading = true
-      console.log(action);
-    })
-    .addCase(filterPrice.fulfilled, (state, action) => {
-      state.loading = false;
-      //console.log(action.payload);
-      state.products = action.payload[2];
-      state.filterPrice = [action.payload[0], action.payload[1]];
-    })
-    .addCase(filterPrice.rejected, (state,action) => {
-      state.loading = false
-      console.error('Error obtaining filtered products ', action.error);
-    })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+        state.filterCategory = false;
+        state.filterPrice = [];
+        state.sort = ['SortR', 'SortP', 'SortN'];
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.loading = false;
+        console.error('Error al obtener los productos:', action.error);
+      })
+      .addCase(getProducts.pending, (state, action) => {
+        state.loading = true;
+        console.log(action);
+      })
+      .addCase(filterCategory.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload[1];
+        state.filterCategory = action.payload[0];
+      })
+      .addCase(filterCategory.rejected, (state, action) => {
+        state.loading = false;
+        console.error('Error obtaining filtered products ', action.error);
+      })
+      .addCase(filterCategory.pending, (state, action) => {
+        state.loading = true;
+        console.log(action);
+      })
+      .addCase(filterPrice.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload[2];
+        state.filterPrice = [action.payload[0], action.payload[1]];
+      })
+      .addCase(filterPrice.rejected, (state, action) => {
+        state.loading = false;
+        console.error('Error obtaining filtered products ', action.error);
+      })
+      .addCase(sortByNameAscending, (state) => {
+        state.products.sort((a, b) => a.name.localeCompare(b.name));
+        state.sort[2] = 'nameDescending'
+      })
+      .addCase(sortByNameDescending, (state) => {
+        state.products.sort((a, b) => b.name.localeCompare(a.name));
+        state.sort[2] = 'nameAscending'
+      })
+      .addCase(sortByPriceAscending, (state) => {
+        state.products.sort((a, b) => b.price-a.price);
+        state.sort[1] = 'priceLowToHigh'
+      })
+      .addCase(sortByPriceDescending, (state) => {
+        state.products.sort((a, b) => a.price-b.price);
+        state.sort[1] = 'priceHighToLow'
+      });
+  },
+});
 
-  }
-})
-//export const {  } = userSlice.actions
-
-export default userSlice.reducer
+export default userSlice.reducer;
