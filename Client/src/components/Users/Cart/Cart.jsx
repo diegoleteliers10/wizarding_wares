86a7c1993c2eb6992cc45ec6c7584bf8404ccca8
Juan Schcolnik <schconlikj@gmail.com>
2023-06-21 11:18:52 -0300

    delete from cart

--- Client/src/components/Users/Cart/Cart.jsx
+++ Client/src/components/Users/Cart/Cart.jsx
@@ -1,5 +1,18 @@
 import Cardd from 'react-bootstrap/Card';
+import { FiTrash2 } from 'react-icons/fi';
+import { useDispatch } from 'react-redux';
+import { removeFromCart } from '../../../redux/userSlice';
+import { useNavigate } from 'react-router-dom';
 const Cart = (props) => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleDelete = async () => {
+        await dispatch(removeFromCart(props.id))
+        navigate('/cart')
+    }
+
     return(
         <div className='storeComponentCard'>
             <Cardd>
@@ -7,6 +20,9 @@ const Cart = (props) => {
                 <h2>{props.name}</h2>
                 <h2>${props.price}</h2>
             </Cardd>
+            <button value={props.productId} onClick={handleDelete} className='button'>
+                <FiTrash2 />
+            </button>
         </div>
     )
 }
