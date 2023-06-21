e5f73b4f3947834a3274cdc47cac63deec2e84e9
marcosmorrone99 <momarcos99@gmail.com>
2023-06-21 13:10:14 -0300

    login con google hecho maestro crack capo

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,11 +5,14 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <Provider store={store}>
   <BrowserRouter>
+  <GoogleOAuthProvider clientId="1029819420881-buu62k2ngdvb25u9r3anfslhc57lfjps.apps.googleusercontent.com">
     <App />
+  </GoogleOAuthProvider>
   </BrowserRouter>
  </Provider>,
 )
