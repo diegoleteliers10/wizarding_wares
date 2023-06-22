6c6485795806929409b641928f76f6cb691d64f8
Juan Schcolnik <schconlikj@gmail.com>
2023-06-22 18:55:58 -0300

    store actualizada con account

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import adminReducer from "./adminSlice"
+import accountReducer from "./accountSlice"
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    admin: adminReducer
+    admin: adminReducer,
+    account: accountReducer
   },
 })
