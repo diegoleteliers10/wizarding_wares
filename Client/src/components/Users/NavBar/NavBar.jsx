e7469a25bfe803b409865114fe16945ddffb5305
Juan Schcolnik <schconlikj@gmail.com>
2023-06-22 18:56:43 -0300

    navBar personalizada

--- Client/src/components/Users/NavBar/NavBar.jsx
+++ Client/src/components/Users/NavBar/NavBar.jsx
@@ -2,10 +2,15 @@ import SearchBar from "../SearchBar/SearchBar"
 import Navbar from 'react-bootstrap/Navbar';
 import '../storeStyles.css'; 
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../../redux/accountSlice";
 
 
 const NavBar = () => {
 
+    const user = useSelector(state => state.account.user)
+
+    const dispatch = useDispatch();
     const navigate = useNavigate()
 
     const handleGoToCart = () => {
@@ -13,6 +18,19 @@ const NavBar = () => {
     }
     const handleGoHome = ()=> {
         navigate('/')
+    }
+
+    const handleLogin = ()=> {
+        navigate('/login')
+    }
+
+    const handleRegister = ()=> {
+        navigate('/register')
+    }
+
+    const handleLogout = () => {
+        dispatch(logOut())
+        navigate("/")
     }
 
     return(
@@ -26,9 +44,21 @@ const NavBar = () => {
             </div>
             <div className="ml-auto buttons">
                 <button onClick={handleGoToCart} className="mx-4 font-semibold text-wwwhite hover:text-wwbeige transition-colors duration-300">Carrito</button>
-                <button className="mx-4 font-semibold text-wwwhite hover:text-wwbeige transition-colors duration-300">Login</button>
+                
+                {user.length === 0 ? 
+                <div>
+                <button onClick={handleLogin} className="mx-4 font-semibold text-wwwhite hover:text-wwbeige transition-colors duration-300">Login</button>
                 <span className="text-wwwhite">|</span>
-                <button className="mx-4 font-semibold text-wwwhite hover:text-wwbeige transition-colors duration-300">Register</button>
+                <button onClick={handleRegister} className="mx-4 font-semibold text-wwwhite hover:text-wwbeige transition-colors duration-300">Register</button>
+                </div>
+                :
+                <div>
+                <span className="text-wwwhite">Welcome back, {user.name} </span>
+                <span className="text-wwwhite">|</span>
+                <button onClick={handleLogout} className="mx-4 font-semibold text-wwwhite hover:text-wwbeige transition-colors duration-300">Cerrar sesión</button>
+                </div>
+                }
+                
             </div>
         </Navbar>
         </div>
