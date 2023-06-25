c54b85f24df3070e98542087548f29c7e745643b
emiferrari2001 <emiferrari2001@gmail.com>
2023-06-24 23:55:18 -0300

    arreglo ruta loginUser

--- Server/src/controllers/loginUser.controller.js
+++ Server/src/controllers/loginUser.controller.js
@@ -1,38 +1,53 @@
-const User = require('../models/Users.model')
-const bcrypt = require("bcrypt");
+const User = require('../models/Users.model');
+const bcrypt = require('bcrypt');
 
-const loginUser = async (req,res)=>{
-  const {email, password, email_verified} = req.body
+const loginUser = async (req, res) => {
+  const { email, password, email_verified } = req.body;
+
   try {
-    const userFound= await User.findOne({
-      where: {
-        email:email
-      }
-    })
 
-    //encriptado de contraseña, para compararla con la encriptacion que tiene el password en la db
-    const salt = await bcrypt.genSalt(10);
-    const passEncrypt = await bcrypt.hash(password, salt);
+    //busco el usuario con el email
+    const userFound = await User.findOne({ where: { email: email } });
 
-
-    if(userFound){ //si encuentra el usuario, que entre al if
-
-      if(userFound.password && userFound.password===passEncrypt){
-
-        res.status(200).json({userInfo:{name:userFound.name,email:userFound.email,id:userFound.id,role:userFound.role},message:"Login Successful"})
-      };
-      
-      if(!userFound.password && email_verified===true){
-        res.status(200).json({userInfo:{name:userFound.name,email:userFound.email,id:userFound.id,role:userFound.role},message:"Login Successful"})
-      }
-
-    }else{
-      res.status(404).json({message:"User not found"})
+    if (!userFound) {
+      //si no lo encuentro retorno error
+      return res.status(404).json({ message: 'User not found' });
     }
-    
-  } catch (error) {
-    res.status(500).json({message:"Something went wrong with the login, try again."})
-  }
-}
+    //si encuentra usuario y ademas recibe propiedad email verified en true, permito acceso
+    if (email_verified === true) {
+      return res.status(200).json({
+        userInfo: {
+          name: userFound.name,
+          email: userFound.email,
+          id: userFound.userId,
+          role: userFound.roleRoleId
+        },
+        message: 'Login Successful'
+      });
+    }
+    if (userFound.password) {
+      // si en vez de email verified recibio password, la encripto y comparo con la guardada en la DB
+      const isPasswordValid = await bcrypt.compare(password, userFound.password);
 
-module.exports= loginUser;
+      if (!isPasswordValid) {
+        // si la password es incorrecta,  devuelve error
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+    }
+    //si la password es correcta, devuelvo info de usuario
+    return res.status(200).json({
+      userInfo: {
+        name: userFound.name,
+        email: userFound.email,
+        id: userFound.userId,
+        role: userFound.roleRoleId
+      },
+      message: 'Login Successful'
+    });
+  } catch (error) {
+    // si hay error en la peticion
+    return res.status(500).json({ message: 'Something went wrong with the login, try again.' });
+  }
+};
+
+module.exports = loginUser;
