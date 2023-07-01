3e6f5b8afedaba83a8f7292e52056117dbb7cc80
Diego Letelier <diegoleteliers10@gmail.com>
2023-07-01 14:18:54 -0400

    fix/loginFix

--- Client/src/components/Users/Login/Login.jsx
+++ Client/src/components/Users/Login/Login.jsx
@@ -67,51 +67,53 @@ const Login = () => {
         <>
         <Toaster/>
         <div className='storeComponent'>
-        <div className='z-10 mx-auto flex items-center justify-center storeLoginForm'>
-        <div className='w-1/2 mx-auto flex items-center justify-center storeLoginForm'>
-            <div>
-                <div className='absolute right-40 -scale-50 flex justify-end'>
-                    <img src="https://miro.medium.com/v2/resize:fit:602/1*2hvodzPPjdPcXpOY-2_IwA.gif" alt="" />
-                </div>
-                <div className='absolute left-40 -z-0 scale-50 flex justify-end'>
-                    <img src="https://miro.medium.com/v2/resize:fit:602/1*2hvodzPPjdPcXpOY-2_IwA.gif" alt="" />
-                </div>
-                
-                <form className="bg-white p-8 rounded shadow-xl max-w-min max-h-max" onSubmit={handleSubmit}>
+            <div className='z-10 mx-auto flex items-center justify-center storeLoginForm'>
+                <div className='w-1/2 mx-auto flex items-center justify-center storeLoginForm'>
                     <div>
-                        {
-                            message && 
-                            <p className='italic text-red-500'>{message}</p>
-                        }
+                        <div className='absolute right-40 -scale-50 flex justify-end'>
+                            <img src="https://miro.medium.com/v2/resize:fit:602/1*2hvodzPPjdPcXpOY-2_IwA.gif" alt="" />
+                        </div>
+                        <div className='absolute left-40 -z-0 scale-50 flex justify-end'>
+                            <img src="https://miro.medium.com/v2/resize:fit:602/1*2hvodzPPjdPcXpOY-2_IwA.gif" alt="" />
+                        </div>
+                        
+                        <form className="bg-white p-8 rounded shadow-xl max-w-min max-h-max" onSubmit={handleSubmit}>
+                            <div>
+                                {
+                                    message && 
+                                    <p className='italic text-red-500'>{message}</p>
+                                }
+                            </div>
+                            <div className='grid fontEB'>
+                                <span className="text-gray-700 text-2xl font-bold uppercase mb-8 fontMarcellus">Iniciar Sesión</span>
+                                <input required type="email" placeholder="Ingrese su email" className="w-full mb-4 h-10 rounded border-gray-300 px-3 focus:border-indigo-500 focus:ring focus:ring-indigo-200 bg-gray-300 " name="email" value={input.email} onChange={handleChange}/>
+                                <input required type="password" placeholder="Ingrese su contraseña" name="password" className="w-full mb-4 h-10 rounded border-gray-300 px-3 focus:border-indigo-500 focus:ring focus:ring-indigo-200 bg-gray-300" value={input.password} onChange={handleChange}/>
+                            </div>
+                            
+                            <button 
+                                className="btn1 btn--svg-small"
+                                type='submit'    
+                            >
+                                Login
+                            </button>
+                        <div className='flex justify-center mt-8'>
+                            <GoogleLogin
+                                theme="light"
+                                onSuccess={handleGoogleLogin}
+                                onError={() => {
+                                    console.log('Login Failed');
+                                }}
+                            />
+                        </div>
+                            <p className='mt-4 fontEB'>¿Aún no tienes cuenta? <span><NavLink to='/register' className='no-underline font-semibold text-wwbrown hover:text-wwmaroon'>Registrarse</NavLink></span></p>                
+                        </form> 
+
                     </div>
-                    <div className='grid fontEB'>
-                        <span className="text-gray-700 text-2xl font-bold uppercase mb-8 fontMarcellus">Iniciar Sesión</span>
-                        <input required type="email" placeholder="Ingrese su email" className="w-full mb-4 h-10 rounded border-gray-300 px-3 focus:border-indigo-500 focus:ring focus:ring-indigo-200 bg-gray-300 " name="email" value={input.email} onChange={handleChange}/>
-                        <input required type="password" placeholder="Ingrese su contraseña" name="password" className="w-full mb-4 h-10 rounded border-gray-300 px-3 focus:border-indigo-500 focus:ring focus:ring-indigo-200 bg-gray-300" value={input.password} onChange={handleChange}/>
-                    </div>
-                    
-                    <button 
-                        className="btn1 btn--svg-small"
-                        type='submit'    
-                    >
-                        Login
-                    </button>
-                <div className='flex justify-center mt-8'>
-                    <GoogleLogin
-                        theme="light"
-                        onSuccess={handleGoogleLogin}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                    />
                 </div>
-                    <p className='mt-4 fontEB'>¿Aún no tienes cuenta? <span><NavLink to='/register' className='no-underline font-semibold text-wwbrown hover:text-wwmaroon'>Registrarse</NavLink></span></p>                
-                </form> 
 
             </div>
         </div>
-
-        </div>
+        </>
     )
 }
 
