6ca9950b111ab8bb65a7efb38e1766779e7a4965
KailyKinG <kailyking@gmail.com>
2023-07-02 22:44:36 -0400

    pruebasVarias(Server/pruebasVarias): cree las notificaciones de cambio de role, suspension de cuenta(Borrado logico), reactivacion de cuenta, notificacion de Cancelacion de cuenta por parte del usuario; cree en la carpeta utils el archivo fechas.js

---
+++ Server/src/utils/fechas.js
@@ -0,0 +1,35 @@
+// Formamos hora, fecha
+  const diasSemana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+  const daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const fecha = new Date;
+
+  module.exports = {
+    diasSemana,
+    daysWeek,
+    fecha,
+  };
+
+  /**
+   * Metodos de fecha
+   * - getFullYear()
+   * - getMonth()
+   * - getDate()
+   * - getHours()
+   * - getMinutes()
+   * - getSeconds()
+   * - getTime(),
+   * - getMilliseconds()
+   * - setFullYear()
+   * - setMonth()
+   * - setDate()
+   * - setHours()
+   * - setMinutes()
+   * - setSeconds()
+   * - setMilliseconds()
+   * - toDateString()
+   * - toTimeString()
+   * - toLocaleDateString()
+   * - toLocaleTimeString()
+   * - toString()
+   * - valueOf()
+   */
