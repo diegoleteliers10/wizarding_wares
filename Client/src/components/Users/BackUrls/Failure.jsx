e31e0448f69021145d2df6170558b9fa1a3d57d7
emiferrari2001 <emiferrari2001@gmail.com>
2023-07-05 15:49:38 -0300

    failure estilos

--- Client/src/components/Users/BackUrls/Failure.jsx
+++ Client/src/components/Users/BackUrls/Failure.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { BiSad } from "react-icons/bi";
 
 const Failure = () => {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Ocurrió un error con la compra <BiSad className="inline-block ml-2" />
+    <main className="grid h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center mx-auto">
+        <img src="https://gamepress.gg/wizardsunite/sites/wizardsunite/files/styles/medium/public/2019-04/Whomping%20Willow-foundable.png?itok=wkiVQcPO" alt="Whomping Willow" className="w-40 mx-auto"/>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl fontMarcellus">
+          Ocurrió un error con la compra
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Intentalo devuelta.
+        <p className="mt-6 text-base leading-7 text-gray-600 fontEB">
+          Inténtalo nuevamente más tarde.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Regresar a la Página Principal
-          </a>
-          <a href="/FAQ" className="text-sm font-semibold text-gray-900">
-            Preguntas Frecuentes <span aria-hidden="true">&rarr;</span>
-          </a>
+        <div className="mt-8 block md:flex items-center justify-center gap-x-6">
+          <div>
+            <a
+              href="/"
+              className="btn1 btn--svg-small"
+            >
+              Regresar a la página principal
+            </a>
+          </div>
+          <div>
+            <a href="/FAQ" className="text-sm font-semibold text-gray-900 fontEB">
+              Preguntas Frecuentes <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
         </div>
       </div>
     </main>
