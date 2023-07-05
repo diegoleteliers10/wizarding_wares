2de2ee1a4886f0658bd9456de960b63b03e4bffa
Diego Letelier <diegoleteliers10@gmail.com>
2023-07-05 12:09:21 -0400

    feat/cambioDeployBack

--- Server/src/db.js
+++ Server/src/db.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = process.env;
+// `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+//la ruta de arriba la cambian por DB_DEPLOY para usar su bd
 
-const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-	{
-		logging: false,
-		native: false,
-	}
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, 
+  native: false, 
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  }
+});
 
-module.exports = sequelize; // Exportamos la coneccion a sequelize
+module.exports = sequelize; // Exportamos la conexion a sequelize
